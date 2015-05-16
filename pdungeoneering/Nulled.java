dcceed07aa2b95e11f083423337e8739b4d4dc4d
agrodon <Paul_feenstra@hotmail.com>
2015-05-17 01:52:50 +0200

    Removing regular logout. Forceful logouts only!

--- pdungeoneering/Nulled.java
+++ pdungeoneering/Nulled.java
@@ -5,7 +5,6 @@ import org.parabot.environment.api.utils.Time;
 import org.parabot.environment.input.Mouse;
 import org.parabot.environment.scripts.framework.Strategy;
 import org.rev317.min.api.methods.Game;
-import org.rev317.min.api.methods.Menu;
 import org.rev317.min.api.methods.Players;
 
 public class Nulled implements Strategy {
@@ -22,9 +21,6 @@ public class Nulled implements Strategy {
 			Mouse.getInstance().click(new Point(380,315));
 			PDungeoneering.nulledBoss = false;
 			PDungeoneering.nulledBossCheck = 0;
-		} else if(!Players.getMyPlayer().isInCombat()){
-			System.out.println("Logging out normally.");
-			Menu.sendAction(315, 1353, 498, 2458, 48613, 1);//logout button
 		} else if ( Players.getMyPlayer().getLocation().getY() > 9000){
 			System.out.println("Forcefully logging out.");
 			PDungeoneering.forceLogout();
