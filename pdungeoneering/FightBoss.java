273683f6f8dc9ac679c7fd6603582c3a3b85c4bd
agrodon <Paul_feenstra@hotmail.com>
2015-05-19 14:25:56 +0200

    More null checks
    NullPointerException check.

--- pdungeoneering/FightBoss.java
+++ pdungeoneering/FightBoss.java
@@ -28,31 +28,31 @@ public class FightBoss implements Strategy {
 				if (Npcs.getNearest(PDungeoneering.BOSS_IDS)[i] != null){
 					final Npc boss = Npcs.getNearest(PDungeoneering.BOSS_IDS)[i];
 					if(!boss.isInCombat()){
-						boss.interact(Npcs.Option.ATTACK);
-						Time.sleep(new SleepCondition(){
-							public boolean isValid() {
-								if(boss != null){
-									if (boss.isInCombat())
-										return true;
+						if(boss != null){
+							boss.interact(Npcs.Option.ATTACK);
+							Time.sleep(new SleepCondition(){
+								public boolean isValid() {
+									if(boss != null){
+										if (boss.isInCombat())
+											return true;
+									}
+									if (Npcs.getNearest(PDungeoneering.THOK_ID) != null){
+										if(Npcs.getNearest(PDungeoneering.THOK_ID).length > 0)
+											return true;
+									}
+									return false;
 								}
-								if (Npcs.getNearest(PDungeoneering.THOK_ID) != null){
-									if(Npcs.getNearest(PDungeoneering.THOK_ID).length > 0)
-										return true;
-								}
-								return false;
-							}
-						},3600);
+							},3600);
+						}
 					}
 					PDungeoneering.nulledBossCheck = 0;
 					//System.out.println("Boss: "+i+" ("+PDungeoneering.BOSS_IDS[i]+ ") "+boss.getLocation()+" animation: "+boss.getAnimation() + "  Health: "+boss.getHealth()+"%");
 				}
 			}
 		}catch(Exception e){
-			if(e.getMessage() != null){//No idea why this should occure, though.
-				if(e.getMessage().contains("0")){
-					//System.out.println("Possible nulled boss? ArrayIndex 0 error");
-					PDungeoneering.nulledBossCheck ++;
-				}
+			if(e.getMessage().contains("0")){
+				//System.out.println("Possible nulled boss? ArrayIndex 0 error");
+				PDungeoneering.nulledBossCheck ++;
 			}
 			Time.sleep(600);
 		}
